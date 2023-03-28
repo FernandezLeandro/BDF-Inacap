eb3d1e562718f9126a30abcf75451b9c51bd9a06
Leandro Fernandez <leanfernandez97@gmail.com>
2023-03-28 12:07:18 -0300

    Add methods getAll, add and deleteByID

--- src/main/java/com/bdf/inacap/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/bdf/inacap/service/impl/CompanyServiceImpl.java
@@ -1,6 +1,54 @@
 package com.bdf.inacap.service.impl;
 
+import com.bdf.inacap.domain.entity.CompanyDE;
+import com.bdf.inacap.repository.CompanyRepository;
+import com.bdf.inacap.rest.controller.dto.CompanyDTO;
 import com.bdf.inacap.service.interfaces.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CompanyServiceImpl implements CompanyService {
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    public CompanyServiceImpl (CompanyRepository companyRepository){
+        this.companyRepository = companyRepository;
+    }
+    @Override
+    public List<CompanyDE> getAll() {
+        return this.companyRepository.findAll();
+    }
+
+    @Override
+    public CompanyDE add(CompanyDE newCompany) {
+        return this.companyRepository.save(newCompany);
+    }
+
+    @Override
+    public CompanyDE deleteByID(Long id) {
+        CompanyDE companyToDelete =  this.getCompanyByID(id);
+        if (!isCompanyNull(companyToDelete)) {
+            this.companyRepository.delete(companyToDelete);
+            //return this.companyMapper.deToDto(companyToDelete);
+        }
+        return companyToDelete; // Agregar excepciones o response entity personalizado (iria en service?)
+    }
+
+    @Override
+    public CompanyDE updateByID(CompanyDE newCompany, Long id) {
+        return null;
+    }
+
+    private CompanyDE getCompanyByID(Long id) {
+        Optional<CompanyDE> company = this.companyRepository.findById(id);
+        return company.isPresent() ? company.get() : null;
+    }
+
+    private boolean isCompanyNull (CompanyDE companyDE){
+        return companyDE == null;
+    }
 }
--- src/main/java/com/bdf/inacap/service/interfaces/CompanyService.java
+++ src/main/java/com/bdf/inacap/service/interfaces/CompanyService.java
@@ -1,4 +1,18 @@
 package com.bdf.inacap.service.interfaces;
 
+import com.bdf.inacap.domain.entity.CompanyDE;
+import com.bdf.inacap.rest.controller.dto.CompanyDTO;
+
+import java.util.List;
+
 public interface CompanyService {
+
+    public List<CompanyDE> getAll ();
+
+    public CompanyDE add (CompanyDE newCompany);
+
+    public CompanyDE deleteByID (Long id);
+
+    public CompanyDE updateByID (CompanyDE newCompany, Long id);
+
 }
