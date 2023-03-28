326d0e2b6b86db4a4b3ebcf069414f56cf2348c9
Leandro Fernandez <leanfernandez97@gmail.com>
2023-03-28 09:41:40 -0300

    Initial commit

---
+++ src/main/java/com/bdf/inacap/InacapApplication.java
@@ -0,0 +1,13 @@
+package com.bdf.inacap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InacapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InacapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdf/inacap/InacapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdf.inacap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InacapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
