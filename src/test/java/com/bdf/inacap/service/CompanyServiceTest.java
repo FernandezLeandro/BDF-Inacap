2acd5520600c21dd4a1bf1ffa70591afdf08a5f9
Leandro Fernandez <leanfernandez97@gmail.com>
2023-03-29 10:37:34 -0300

    Refactor : Now we use EasyRandom to generate objets

--- src/test/java/com/bdf/inacap/service/CompanyServiceTest.java
+++ src/test/java/com/bdf/inacap/service/CompanyServiceTest.java
@@ -3,6 +3,7 @@ package com.bdf.inacap.service;
 import com.bdf.inacap.domain.entity.CompanyDE;
 import com.bdf.inacap.repository.CompanyRepository;
 import com.bdf.inacap.service.impl.CompanyServiceImpl;
+import org.jeasy.random.EasyRandom;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,16 +28,23 @@ public class CompanyServiceTest {
     private CompanyServiceImpl companyService;
 
     private CompanyDE company;
+    private EasyRandom generator;
+
     @BeforeEach
     public void setUp (){
-        this.company = new CompanyDE();
-        this.company.id = 1L;
-        this.company.name = "BDF";
-        this.company.estadoAlta = true;
+        this.generator = new EasyRandom();
+        this.company = generator.nextObject(CompanyDE.class);
+
+//        this.company = new CompanyDE();
+//        this.company.id = 1L;
+//        this.company.name = "BDF";
+//        this.company.estadoAlta = true;
     }
 
     @Test
     public void shouldReturnAllCompanies() {
+        this.company.estadoAlta = true;
+
         when(this.companyRepository.findAll()).thenReturn(Arrays.asList(company));
 
         assertEquals(this.companyService.getAll(), Arrays.asList(company));
@@ -44,9 +53,11 @@ public class CompanyServiceTest {
     @Test
     public void shouldFailToReturnAllCompanies() {
         this.company.estadoAlta= false;
-        CompanyDE [] companies = {};
+        CompanyDE [] companiesEmpty = {};
+
         when(this.companyRepository.findAll()).thenReturn(Arrays.asList(company));
-        assertEquals(this.companyService.getAll(),List.of(companies));
+
+        assertEquals(this.companyService.getAll(),List.of(companiesEmpty));
     }
 
 }
