1274d36ecfb85dcefa0a38bb7023010eaac688a3
Leandro Fernandez <leanfernandez97@gmail.com>
2023-04-03 08:58:26 -0300

    Add little change

--- src/main/java/com/bdf/inacap/domain/entity/ZipCode.java
+++ src/main/java/com/bdf/inacap/domain/entity/ZipCode.java
@@ -8,11 +8,6 @@ import lombok.Data;
 
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Transient;
-
 @SuppressWarnings("serial")
 @Entity
 @Data
